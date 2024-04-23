3205b22cec5a6a3b3658ecf973b450c8dc298804
NataliaSoazo <natalia.s.laboralmail.com>
2024-04-23 14:40:48 -0300

    m

--- obj/Debug/net8.0/INMO-SOAZO_2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/INMO-SOAZO_2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INMO-SOAZO_2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e260d192d9eddd75f7a9329e589b4ff17d7a28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3489347b5543645d427faf89ac1d24ff09ef67c")]
 [assembly: System.Reflection.AssemblyProductAttribute("INMO-SOAZO_2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INMO-SOAZO_2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
